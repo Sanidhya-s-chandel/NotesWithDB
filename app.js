9257435663300876249ab36d3d09d6e3fb8b844f
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-12-04 23:10:18 +0530

    Complete CRUD

--- app.js
+++ app.js
@@ -42,6 +42,29 @@ app.get("/delete/:id", async (req,res)=>{
     res.redirect("/");
 }); 
 
+app.get("/update/:id", async (req,res)=>{
+   
+    let file = await fileModel.findOne({_id : req.params.id})
+    res.render("update",{file});
+
+});
+
+app.post("/update/:id", async (req,res)=>{
+
+   const {fileName,description,data} = req.body;
+   
+   let result = await fileModel.updateOne(
+
+    {_id : req.params.id},
+    {$set: {fileName,description,data}});
+
+   if (result.modifiedCount > 0){
+    res.redirect("/");
+   } else  {
+    res.status(400).send("No changes were made to the file");}
+
+});
+
 app.listen(3000,()=>{
     console.log("The Server is running at Port: 3000");
 });
